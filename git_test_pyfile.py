7d3e9371154e95c12db528b6969ee491ecab4e7f
hannesbachmann <hannes-bachmann@t-online.de>
2020-03-25 14:02:50 +0100

    "testing to import some modules"

---
+++ git_test_pyfile.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import sklearn
+import pyautogui as auto
+import math
+import random
+import time
+import tkinter as tk
+import selenium
+import kivy
+import keras
+import tensorflow as tf
+import tensorboard as tb
+import twisted
+import multiprocessing
+import threading
+import pygame
+import scipy
+import errno
+import cv2
+import pickle
+import socket
+import socketserver
+import json
